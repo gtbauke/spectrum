229f2600132cf6f142a5a35f5cbfb055566de181
Gustavo Bauke <baukegustavo@gmail.com>
2025-07-31 23:19:12 -0300

    frontend: adds basic job header component

--- frontend/src/pages/dataset.page.tsx
+++ frontend/src/pages/dataset.page.tsx
@@ -1,3 +1,4 @@
+import { IoCopy, IoPencil, IoTrash } from "react-icons/io5";
 import { useParams } from "react-router";
 
 type DatasetPageParams = {
@@ -10,6 +11,36 @@ export function DatasetPage() {
     return (
         <div>
             <h1 className="text-white">Dataset - {datasetId}</h1>
+
+            {/* TODO: move this to a Job component */}
+            <div className="flex bg-background-secondary p-2 rounded">
+                <div className="flex flex-row items-center justify-between w-full">
+                    <p className="text-white">Job ID - Job Name</p>
+
+                    <div className="flex flex-row items-center">
+                        <button type="button" className="cursor-pointer p-2">
+                            <IoPencil
+                                size={20}
+                                className="text-white hover:text-gray-300 active:text-blue-300"
+                            />
+                        </button>
+
+                        <button type="button" className="cursor-pointer p-2">
+                            <IoCopy
+                                size={20}
+                                className="text-white hover:text-gray-300 active:text-green-300"
+                            />
+                        </button>
+
+                        <button type="button" className="cursor-pointer p-2">
+                            <IoTrash
+                                size={20}
+                                className="text-white hover:text-gray-300 active:text-red-300"
+                            />
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
