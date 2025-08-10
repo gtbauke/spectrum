84e3571ef56cd096aee4f9807f80bb454e25a38e
Gustavo Bauke <baukegustavo@gmail.com>
2025-08-09 23:55:41 -0300

    backend: fix Config error

--- backend/app/utils/config.py
+++ backend/app/utils/config.py
@@ -2,9 +2,9 @@ from os import getenv
 
 
 class Config:
-    REDIS_URL = getenv("REDIS_URL", "redis://localhost:6379/0")
-    POSTGRES_URL = getenv(
-        "POSTGRES_URL", "postgresql://postgres:postgres@localhost/spectrum")
+    REDIS_URL: str = getenv("REDIS_URL", "redis://localhost:6379/0")
+    DATABASE_URL: str = getenv(
+        "DATABASE_URL", "postgresql://postgres:postgres@localhost/spectrum")
 
     CELERY_BROKER_URL = REDIS_URL
     CELERY_RESULT_BACKEND = REDIS_URL
