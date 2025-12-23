29adbbc679a214adb1226c91c594254836f93137
Gustavo Bauke <baukegustavo@gmail.com>
2025-12-23 00:45:14 -0300

    scripts: adds script to generate random dataset

---
+++ scripts/create-random-dataset.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+
+def main():
+    num_features = int(input("Enter the number of features: "))
+    num_rows = int(input("Enter the number of rows: "))
+
+    columns = ["target"] + [f"feature{i}" for i in range(1, num_features + 1)]
+    df = pd.DataFrame(columns=columns)
+
+    for col in columns:
+        df[col] = pd.Series(np.random.rand(num_rows))
+
+    df.to_csv("examples/random_dataset.csv", index=False)
+
+
+main()
