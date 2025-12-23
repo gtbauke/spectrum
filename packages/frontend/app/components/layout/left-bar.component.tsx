8caa4a6f7c10d589a9043edc4447616d27329509
Gustavo Bauke <baukegustavo@gmail.com>
2025-12-23 14:33:10 -0300

    frontend: increases aside area width in left bar layout component

--- packages/frontend/app/components/layout/left-bar.component.tsx
+++ packages/frontend/app/components/layout/left-bar.component.tsx
@@ -10,7 +10,7 @@ export type LeftBatLayoutProps = {
 export function LeftBarLayout({ Aside, Main, mainProps }: LeftBatLayoutProps) {
     return (
         <main className="flex h-screen">
-            <aside className="min-w-64 shadow-lg">
+            <aside className="min-w-72 shadow-lg">
                 <Aside />
             </aside>
             <div className="flex-1">
