c2a37fff721c0b537fef6c2fd767eff08a08611e
Gustavo Bauke <baukegustavo@gmail.com>
2025-12-23 20:01:57 -0300

    shared: adds dataset description property

--- packages/shared/src/datasets.validator.ts
+++ packages/shared/src/datasets.validator.ts
@@ -1,10 +1,24 @@
 import { z } from "zod";
 
 export const datasetValidator = z.object({
-    id: z.cuid(),
+    id: z.cuid({
+        error: "Id must be a valid CUID",
+    }),
 
-    title: z.string(),
-    fileUrl: z.url(),
+    title: z
+        .string({
+            error: "Title must be a string",
+        })
+        .min(1, {
+            error: "Title is required",
+        }),
+    description: z.string({
+        error: "Description must be a string",
+    }),
+
+    fileUrl: z.url({
+        error: "File URL must be a valid URL",
+    }),
 
     createdAt: z.date(),
     updatedAt: z.date(),
@@ -12,6 +26,7 @@ export const datasetValidator = z.object({
 
 export const createDatasetValidator = datasetValidator.pick({
     title: true,
+    description: true,
 });
 
 export type CreateDatasetData = z.infer<typeof createDatasetValidator>;
