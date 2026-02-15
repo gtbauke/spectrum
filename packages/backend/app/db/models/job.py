fce54c7ecf5400ab98b1cc0d3dab428b4b4b9b2b
Gustavo T. Bauke <baukegustavo@gmail.com>
2026-02-14 22:57:28 -0300

    backend: adds model hyperparameters to Job class

--- packages/backend/app/db/models/job.py
+++ packages/backend/app/db/models/job.py
@@ -3,14 +3,15 @@ from __future__ import annotations
 import uuid
 
 from datetime import datetime
-from typing import TYPE_CHECKING
-from sqlalchemy import DateTime, Enum, func, ForeignKey
+from typing import TYPE_CHECKING, Optional
+from sqlalchemy import DateTime, Enum, func, ForeignKey, Integer, String, Boolean, Float
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.base import Base
 from app.domain.jobs.job_status import JobStatus
 from app.domain.jobs.job import Job
+from app.domain.jobs.loss_function import LossFunction
 
 if TYPE_CHECKING:
     from app.db.models.dataset import DatasetORM
@@ -58,16 +59,97 @@ class JobORM(Base):
         default=func.now(),
     )
 
-    started_at: Mapped[datetime | None] = mapped_column(
+    started_at: Mapped[Optional[datetime]] = mapped_column(
         DateTime(timezone=True),
         nullable=True,
         default=None,
     )
 
-    finished_at: Mapped[datetime | None] = mapped_column(
+    finished_at: Mapped[Optional[datetime]] = mapped_column(
         DateTime(timezone=True),
         nullable=True,
         default=None,
+    )
+
+    generations: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+        default=100,
+    )
+
+    population: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+        default=100,
+    )
+
+    max_size: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+        default=15,
+    )
+
+    number_of_tournaments: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+        default=3,
+    )
+
+    crossover_probability: Mapped[float] = mapped_column(
+        Float,
+        nullable=False,
+        default=0.9,
+    )
+
+    mutation_probability: Mapped[float] = mapped_column(
+        Float,
+        nullable=False,
+        default=0.3,
+    )
+
+    non_terminals: Mapped[str] = mapped_column(
+        String,
+        nullable=False,
+        default="add,sub,mul,div",
+    )
+
+    loss: Mapped[LossFunction] = mapped_column(
+        Enum(
+            LossFunction,
+            name="loss_function",
+        ),
+        nullable=False,
+        default=LossFunction.MSE,
+    )
+
+    optimization_iterations: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+        default=50,
+    )
+
+    optimization_repeats: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+        default=2,
+    )
+
+    max_param_count: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+        default=-1,
+    )
+
+    split: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+        default=1,
+    )
+
+    simplify: Mapped[bool] = mapped_column(
+        Boolean,
+        nullable=False,
+        default=False,
     )
 
     def to_domain(self) -> Job:
@@ -79,6 +161,19 @@ class JobORM(Base):
             started_at=self.started_at,
             finished_at=self.finished_at,
             models=[model.to_domain() for model in self.models],
+            generations=self.generations,
+            population=self.population,
+            max_size=self.max_size,
+            number_of_tournaments=self.number_of_tournaments,
+            crossover_probability=self.crossover_probability,
+            mutation_probability=self.mutation_probability,
+            non_terminals=self.non_terminals,
+            loss=self.loss,
+            optimization_iterations=self.optimization_iterations,
+            optimization_repeats=self.optimization_repeats,
+            max_param_count=self.max_param_count,
+            split=self.split,
+            simplify=self.simplify,
         )
 
     @classmethod
@@ -91,4 +186,17 @@ class JobORM(Base):
             started_at=job.started_at,
             finished_at=job.finished_at,
             models=[ModelORM.from_domain(model) for model in job.models],
+            generations=job.generations,
+            population=job.population,
+            max_size=job.max_size,
+            number_of_tournaments=job.number_of_tournaments,
+            crossover_probability=job.crossover_probability,
+            mutation_probability=job.mutation_probability,
+            non_terminals=job.non_terminals,
+            loss=job.loss,
+            optimization_iterations=job.optimization_iterations,
+            optimization_repeats=job.optimization_repeats,
+            max_param_count=job.max_param_count,
+            split=job.split,
+            simplify=job.simplify,
         )
--- packages/backend/app/domain/jobs/job.py
+++ packages/backend/app/domain/jobs/job.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from typing import TYPE_CHECKING
 
 from app.domain.jobs.job_status import JobStatus
+from app.domain.jobs.loss_function import LossFunction
 
 if TYPE_CHECKING:
     from app.domain.datasets.dataset import Dataset
@@ -30,6 +31,33 @@ class Job(BaseModel):
     models: list["Model"] = Field(...,
                                   description="The list of models produced by the job")
 
+    generations: int = Field(...,
+                             description="The number of generations for the genetic algorithm")
+    population: int = Field(...,
+                            description="The population size for the genetic algorithm")
+    max_size: int = Field(...,
+                          description="The maximum size of the expression")
+    number_of_tournaments: int = Field(
+        ..., description="The number of tournaments for the genetic algorithm")
+    crossover_probability: float = Field(
+        ..., description="The crossover probability for the genetic algorithm")
+    mutation_probability: float = Field(
+        ..., description="The mutation probability for the genetic algorithm")
+    non_terminals: str = Field(
+        ..., description="The non-terminals to be used in the genetic programming")
+    loss: LossFunction = Field(
+        ..., description="The loss function to be used for evaluating the models")
+    optimization_iterations: int = Field(
+        ..., description="The number of optimization iterations for the models")
+    optimization_repeats: int = Field(
+        ..., description="The number of optimization repeats for the models")
+    max_param_count: int = Field(
+        ..., description="The maximum number of parameters for the models")
+    split: int = Field(...,
+                       description="The split to be used for training and testing the models")
+    simplify: bool = Field(...,
+                           description="Whether to simplify the expressions of the models")
+
     @classmethod
     def new(cls, dataset: "Dataset") -> Job:
         return cls(
@@ -40,4 +68,17 @@ class Job(BaseModel):
             started_at=None,
             finished_at=None,
             models=[],
+            generations=100,
+            population=100,
+            max_size=15,
+            number_of_tournaments=3,
+            crossover_probability=0.9,
+            mutation_probability=0.3,
+            non_terminals="add,sub,mul,div",
+            loss=LossFunction.MSE,
+            optimization_iterations=50,
+            optimization_repeats=2,
+            max_param_count=-1,
+            split=1,
+            simplify=False,
         )
