31bda77752b14e56a226eda3b426a5c114074dc6
Gustavo T. Bauke <baukegustavo@gmail.com>
2026-02-18 23:23:55 -0300

    (workers): adds Consumer abstraction

---
+++ packages/workers/workers/consumers/base.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from types import TracebackType
+from typing import Optional, Type
+
+from pydantic import BaseModel
+from db.session import AsyncSessionLocal
+
+from workers.common.get_uow import WorkerUnitOfWork
+from workers.infra.rabbitmq.setup import setup_all
+from workers.infra.rabbitmq.session import create_connection
+
+
+class AbstractConsumer[Event: BaseModel](ABC):
+    async def __aenter__(self):
+        self._connection, self._channel = await create_connection()
+        await setup_all(self._channel)
+
+        self._unit_of_work = WorkerUnitOfWork(AsyncSessionLocal)
+        self._uow = await self._unit_of_work.__aenter__()
+
+        return self
+
+    async def __aexit__(self, exc_type: Optional[Type[BaseException]],
+                        exc: Optional[BaseException],
+                        tb: Optional[TracebackType],):
+        await self._unit_of_work.__aexit__(exc_type, exc, tb)
+
+        await self._channel.close()
+        await self._connection.close()
+
+    @property
+    def outbox(self):
+        return self._uow.outbox
+
+    @abstractmethod
+    async def consume(self, event: Event): ...
--- packages/workers/workers/consumers/outbox_events/publish_outbox_events.py
+++ packages/workers/workers/consumers/outbox_events/publish_outbox_events.py
@@ -1,38 +1,34 @@
 import logging
 import asyncio
 
+from pydantic import BaseModel
+
+from core.models.outbox.outbox import Outbox
+
 from workers.common.sleep import IDLE_SLEEP, ACTIVE_SLEEP
-from workers.common.get_uow import get_uow
-from workers.infra.rabbitmq.setup import setup_all
-from workers.infra.rabbitmq.session import create_connection
+from workers.consumers.base import AbstractConsumer
 
 
 logger = logging.getLogger(__name__)
 
-# TODO: I copied the OutboxORM model from the backend package, we should
-# consider moving it to the core package to avoid duplication and ensure consistency
+
+class OutboxConsumer(AbstractConsumer[Outbox[BaseModel]]):
+    async def consume(self, event: Outbox[BaseModel]):
+        logger.info(f"Consuming outbox event {event.id}")
 
 
 async def publish_outbox_events():
-    connection, channel = await create_connection()
-    await setup_all(channel)
-
     shutdown_event = asyncio.Event()
 
     while not shutdown_event.is_set():
-        async with get_uow() as uow:
-            events = await uow.outbox.fetch_batch()
+        async with OutboxConsumer() as consumer:
+            events = await consumer.outbox.fetch_batch()
 
             if len(events) == 0:
                 await asyncio.sleep(IDLE_SLEEP)
                 continue
 
             for event in events:
-                # TODO: publish event to message broker
-                logger.info(f"Publishing event {event.id} to message broker")
+                await consumer.consume(event)
 
-            await uow.commit()
             await asyncio.sleep(ACTIVE_SLEEP)
-
-    await channel.close()
-    await connection.close()
