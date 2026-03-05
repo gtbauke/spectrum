aee9d61e439c8f08e3e921cb109a886cf9074353
Gustavo T. Bauke <baukegustavo@gmail.com>
2026-03-05 17:17:30 -0300

    (backend): changes InvalidCredentialsException to be an HTTPException

--- packages/backend/app/features/auth/errors/invalid_credentials.py
+++ packages/backend/app/features/auth/errors/invalid_credentials.py
@@ -1,3 +1,8 @@
-class InvalidCredentials(Exception):
+from fastapi import HTTPException, status
+
+
+class InvalidCredentials(HTTPException):
     """Raised when the provided credentials are invalid."""
-    pass
+
+    def __init__(self, detail: str = "Invalid credentials"):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail)
