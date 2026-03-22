c6cd682b23120ea2406e00112c746b90b1f26ed3
Gustavo T. Bauke <baukegustavo@gmail.com>
2026-03-22 12:32:18 -0300

    (backend): adds BufferedEventsPublisher in UoW

--- packages/backend/app/adapters/unit_of_work.py
+++ packages/backend/app/adapters/unit_of_work.py
@@ -2,10 +2,14 @@ from core.ports.unit_of_work import UnitOfWork
 from db.adapters.unit_of_work import SqlAlchemyUnitOfWork
 
 from .storage.local_storage import LocalStorage
+from .events.buffered_events_publisher import BufferedEventsPublisher
 
 
 class ApiUnitOfWork(SqlAlchemyUnitOfWork):
     async def __aenter__(self) -> UnitOfWork:
-        self.file_storage = LocalStorage()
+        await super().__aenter__()
 
-        return await super().__aenter__()
+        self.file_storage = LocalStorage()
+        self.events_publisher = BufferedEventsPublisher(self._broker)
+
+        return self
